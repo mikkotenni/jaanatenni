143451610439f3b9f933deb18fd2f3157e7987cb
mikkotenni <mikko.tenni@hotmail.com>
2024-03-28 18:06:09 +0200

    feat: app colors

---
+++ src/assets/designTokens.js
@@ -0,0 +1,18 @@
+/**
+ * Colors used in the app.
+ * @type {Object}
+ * @property {string} color1 - Very dark grayish blue
+ * @property {string} color2 - Light grayish orange
+ * @property {string} color3 - Grayish blue
+ * @property {string} color4 - Light grayish orange
+ * @property {string} color5 - Light grayish cyan
+ * @example
+ * import { colors } from './src/assets/colors';
+ */
+export const colors = {
+  color1: "#475e67", // Very dark grayish blue
+  color3: "#a4b4be", // Grayish blue
+  color4: "#dcc8bc", // Light grayish orange
+  color2: "#eae1d0", // Light grayish orange
+  color5: "#e0edee", // Light grayish cyan
+};
