2171136ce927e8ae5d2d3d895921f35d23653543
mikkotenni <mikko.tenni@hotmail.com>
2024-03-28 18:37:19 +0200

    feat: Emotions and basic global styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Global, css } from "@emotion/react";
+import { colors } from "./assets/designTokens";
+
+const { fontColor, backgroundColor } = colors;
+
+const globalStyles = css`
+  html,
+  body {
+    font-family: "Raleway", sans-serif;
+    font-optical-sizing: auto;
+    font-weight: 400;
+    font-style: normal;
+    font-size: 16px;
+    line-height: 1.5;
+    color: ${fontColor};
+    background-color: ${backgroundColor};
+    margin: 0;
+  }
+`;
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Global styles={globalStyles} />
+      <p>Hello World!</p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
