f02843ec6d427995e1225d2bd5c35af06a3753a0
mikkotenni <mikko.tenni@hotmail.com>
2024-03-28 20:58:15 +0200

    feat: Hero

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Global, css } from "@emotion/react";
 import { colors } from "./assets/designTokens";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import Hero from "./components/Hero";
 
 const { fontColor, backgroundColor } = colors;
 
@@ -25,7 +26,7 @@ function App() {
     <>
       <Global styles={globalStyles} />
       <Header />
-      <p>Hello World!</p>
+      <Hero />
       <Footer />
     </>
   );
---
+++ src/components/Hero.jsx
@@ -0,0 +1,98 @@
+import styled from "@emotion/styled";
+import { breakpoints, colors } from "../assets/designTokens";
+import Mailto from "./reachOutLinks/Mailto";
+import Tel from "./reachOutLinks/Tel";
+import WhatsApp from "./reachOutLinks/WhatsApp";
+import chaiseLongue from "../assets/chaise_longue.webp";
+
+const { md } = breakpoints;
+const { color1, color2, color3, color5 } = colors;
+
+const Container = styled.section`
+  background-image: url(${chaiseLongue});
+  background-repeat: no-repeat;
+  background-position: center center;
+  background-size: cover;
+  padding: 1rem;
+  transition: padding 0.5s ease-out;
+  background-color: ${color3};
+
+  @media (min-width: ${md}) {
+    padding: 6rem 3rem;
+  }
+
+  h1 {
+    margin-top: 0;
+    margin-bottom: 0.75rem;
+    background-color: ${color2};
+    padding: 1rem 1.5rem;
+    font-size: 1.25rem;
+    display: inline-block;
+    border-top-left-radius: 1rem;
+    border-top-right-radius: 1rem;
+    border-bottom-right-radius: 1rem;
+    margin-right: 3rem;
+
+    @media (min-width: ${md}) {
+      font-size: 2.25rem;
+    }
+  }
+
+  p {
+    background-color: ${color5};
+    padding: 1rem 1.5rem;
+    display: inline-block;
+    max-width: 900px;
+    border-top-left-radius: 1rem;
+    border-top-right-radius: 1rem;
+    border-bottom-left-radius: 1rem;
+    font-weight: 600;
+    margin-left: 3rem;
+    margin-top: 0;
+    margin-bottom: 0.75rem;
+    color: ${color1};
+
+    @media (min-width: ${md}) {
+      font-size: 1.25rem;
+    }
+
+    & a {
+      display: block;
+      padding-top: 0.25rem;
+      padding-bottom: 0.25rem;
+
+      @media (min-width: ${md}) {
+        display: inline-block;
+      }
+    }
+
+    & a:not(:last-of-type) {
+      @media (min-width: ${md}) {
+        padding-right: 2rem;
+      }
+    }
+  }
+`;
+
+export default function Hero() {
+  return (
+    <Container>
+      <h1>Mielenterveyttä ja hyvinvointia lyhytterapialla</h1>
+      <div>
+        <p>
+          Ratkaisuja ja voimavaroja elämän eri haasteisiin. Mielenterveyttä ja
+          psyykkistä hyvinvointia kannattaa vaalia jo ennen kuin mitään
+          suurempia haasteita on ilmaantunutkaan. Varaa ilmainen 15 minuutin
+          tutustuminen!
+        </p>
+      </div>
+      <div>
+        <p>
+          <WhatsApp />
+          <Tel />
+          <Mailto />
+        </p>
+      </div>
+    </Container>
+  );
+}
