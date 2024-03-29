6f7e97b3ad9683cac94c0ee443f72a19f6812690
mikkotenni <mikko.tenni@hotmail.com>
2024-03-29 09:32:10 +0200

    feat: add contact information component

---
+++ src/components/ContactInformation.jsx
@@ -0,0 +1,36 @@
+import Mailto from "./reachOutLinks/Mailto";
+import Tel from "./reachOutLinks/Tel";
+import WhatsApp from "./reachOutLinks/WhatsApp";
+import styled from "@emotion/styled";
+
+const Container = styled.section`
+  margin-top: 2rem;
+
+  & > h2:first-of-type {
+    margin-top: 0;
+  }
+`;
+
+export default function ContactInformation() {
+  return (
+    <Container id="yhteystiedot">
+      <h2>Yhteystiedot</h2>
+      <p>
+        Järjestän terapiatapaamisemme etäyhteydellä, luonasi tai
+        kotivastaanotollani Mäntsälässä. Lähitapaamiset sopivat siis erityisesti
+        sinulle, joka asut Uudellamaalla, Kanta-Hämeessä tai Päijät-Hämeessä.
+        Etätapaamiset sopivat ihan kaikille.
+      </p>
+      <p>Ota yhteyttä ja varaa aikasi nyt.</p>
+      <p>
+        <WhatsApp />
+      </p>
+      <p>
+        <Tel />
+      </p>
+      <p>
+        <Mailto />
+      </p>
+    </Container>
+  );
+}
