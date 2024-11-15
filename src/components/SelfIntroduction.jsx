0f941fed2760e9b998a93fe7e5aa94bee573ef35
mikkotenni <mikko.tenni@hotmail.com>
2024-11-15 20:07:05 +0200

    feat: more control from CMS

--- src/components/SelfIntroduction.jsx
+++ src/components/SelfIntroduction.jsx
@@ -1,7 +1,9 @@
+import { useEffect, useState } from "react";
 import styled from "@emotion/styled";
 import Picture from "./Picture";
 import Jaana from "../assets/jaana.webp";
 import JaanaMobile from "../assets/jaanamobile.webp";
+import { getPosts } from "../api/apiFunctions";
 
 const Container = styled.section`
   margin-top: 2rem;
@@ -11,16 +13,31 @@ const Container = styled.section`
   }
 `;
 
+const fallbackContent = new Map([
+  ["Heading", "Jaana"],
+  [
+    "Introduction",
+    "Hei! Olen valmistunut lyhytterapeutti ja psykoterapeuttiopiskelija. Olen työskennellyt lasten, nuorten ja perheiden parissa sekä mielenterveysalalla. Erityisenä mielenkiinnon kohteena ja lisäkoulutuksen aiheena on ollut trauma- ja kiintymyssuhdetyöskentely.",
+  ],
+]);
+
 export default function SelfIntroduction() {
+  const [content, setContent] = useState(new Map());
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const posts = await getPosts(4);
+      setContent(posts);
+    };
+
+    fetchPosts();
+  }, []);
+
   return (
     <Container id="jaana">
-      <h2>Jaana</h2>
+      <h2>{content.get("Heading") || fallbackContent.get("Heading")}</h2>
       <p>
-        Hei! Olen lyhytterapeutti, psykoterapeuttiopiskelija
-        sekä terveydenhuolto- ja sosiaalialan ammattilainen. Olen työskennellyt
-        lasten, nuorten ja perheiden parissa sekä mielenterveysalalla. Erityisenä
-        mielenkiinnon kohteena ja lisäkoulutuksen aiheena on ollut trauma- ja
-        kiintymyssuhdetyöskentely.
+        {content.get("Introduction") || fallbackContent.get("Introduction")}
       </p>
       <Picture mobileSrc={JaanaMobile} src={Jaana} alt="Jaana" />
     </Container>
--- src/components/TherapyConcept.jsx
+++ src/components/TherapyConcept.jsx
@@ -1,5 +1,7 @@
+import { useEffect, useState } from "react";
 import styled from "@emotion/styled";
 import { colors } from "../assets/designTokens";
+import { getPosts } from "../api/apiFunctions";
 
 const { tagColor } = colors;
 
@@ -17,37 +19,69 @@ const Container = styled.section`
   }
 `;
 
+const fallbackContent = new Map([
+  ["Heading", "Lyhytterapia"],
+  [
+    "Concept",
+    "Lyhytterapia on tutkitusti tehokas tapa tukea mielenterveyttä erilaisissa elämäntilanteissa ja mielen hyvinvoinnin haasteissa. Ratkaisukeskeinen lyhytterapia keskittyy tulevaisuuteen ja voimavaroihin. Terapian kestoa ei sovita etukäteen, vaan tilannetta arvioidaan yhdessä asiakkaan kanssa jokaisella tapaamiskerralla.",
+  ],
+]);
+
 export default function TherapyConcept() {
+  const [content, setContent] = useState(new Map());
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const posts = await getPosts(5);
+      setContent(posts);
+    };
+
+    fetchPosts();
+  }, []);
+
   return (
     <Container id="lyhytterapia">
-      <h2>Lyhytterapia</h2>
+      <h2>{content.get("Heading") || fallbackContent.get("Heading")}</h2>
       <p>
-        Lyhytterapia on tutkitusti tehokas tapa tukea mielenterveyttä
-        erilaisissa elämäntilanteissa ja mielen hyvinvoinnin haasteissa. Ratkaisukeskeinen
-        lyhytterapia keskittyy tulevaisuuteen ja voimavaroihin. Terapian kestoa
-        ei sovita etukäteen, vaan tilannetta arvioidaan yhdessä asiakkaan kanssa
-        jokaisella tapaamiskerralla.
+        {content.get("Concept") || fallbackContent.get("Concept")}
       </p>
       <p>
-        <span className="material-symbols-outlined" aria-hidden="true"> check </span> Pidämme
-        terapiatapaamiset joko etänä, sinun luonasi tai vasta valmistuneella kotivastaanotollani.
+        <span className="material-symbols-outlined" aria-hidden="true">
+          {" "}
+          check{" "}
+        </span>{" "}
+        Pidämme terapiatapaamiset joko etänä, sinun luonasi tai vasta
+        valmistuneella kotivastaanotollani.
       </p>
       <p>
-        <span className="material-symbols-outlined" aria-hidden="true"> check </span> Kävelyterapia
-        on hauska vaihtoehto, joko samalla lenkkipolulla tai etänä
+        <span className="material-symbols-outlined" aria-hidden="true">
+          {" "}
+          check{" "}
+        </span>{" "}
+        Kävelyterapia on hauska vaihtoehto, joko samalla lenkkipolulla tai etänä
         kuulokkeiden kautta.
       </p>
       <p>
-        <span className="material-symbols-outlined" aria-hidden="true"> check </span> Terapiaan
-        voit tulla yksin, kumppanisi kanssa tai vaikka koko perheen voimin.
+        <span className="material-symbols-outlined" aria-hidden="true">
+          {" "}
+          check{" "}
+        </span>{" "}
+        Terapiaan voit tulla yksin, kumppanisi kanssa tai vaikka koko perheen
+        voimin.
       </p>
       <p>
-        <span className="material-symbols-outlined" aria-hidden="true"> check </span> Työni tueksi
-        saan itse säännöllisesti työnohjausta.
+        <span className="material-symbols-outlined" aria-hidden="true">
+          {" "}
+          check{" "}
+        </span>{" "}
+        Työni tueksi saan itse säännöllisesti työnohjausta.
       </p>
       <p>
-        <span className="material-symbols-outlined" aria-hidden="true"> check </span> Etäyhteys on
-        helppo käyttää.
+        <span className="material-symbols-outlined" aria-hidden="true">
+          {" "}
+          check{" "}
+        </span>{" "}
+        Etäyhteys on helppo käyttää.
       </p>
     </Container>
   );
