39af9de79619b73613f8a24913781c330a48d917
mikkotenni <mikko.tenni@hotmail.com>
2024-11-19 14:25:20 +0200

    feat: a single content fetch

--- src/api/apiFunctions.js
+++ src/api/apiFunctions.js
@@ -1,18 +1,38 @@
 import { stripHtml } from "../utils/formatting.js";
 
 /**
- * Fetches posts from the Wordpress CMS.
- * @param {number} category in Wordpress
- * @returns {Promise<Map<string, string>>} as posts
+ * Fetches posts from the Wordpress CMS and returns them as a map of titles and content.
+ * If a category ID is provided, filters posts by the specified category.
+ *
+ * @param {number} [category] - The ID of the category in Wordpress to filter posts by.
+ * @returns {Promise<Map<string, string>>} - A promise that resolves to a Map where each key is a post title, and the value is the post content.
+ *
+ * @throws {Error} If the content could not be fetched from the CMS.
  */
 export async function getPosts(category) {
-  const response = await fetch("https://cms.jaanatenni.fi/wp-json/wp/v2/posts");
-  if (!response.ok) {
-    console.error(`Content could not be fetched: ${response.status}`);
-    return new Map();
+  // Using Promise object to make sure that subsequent function calls don't fetch again.
+  if (!window.contentPromise) {
+    window.contentPromise = fetch(
+      "https://cms.jaanatenni.fi/wp-json/wp/v2/posts"
+    )
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`Content could not be fetched: ${response.status}`);
+        }
+        return response.json();
+      })
+      .then((data) => {
+        window.content = { posts: data };
+        return data;
+      })
+      .catch((error) => {
+        console.error(error);
+        window.content = { posts: [] };
+        return [];
+      });
   }
 
-  let posts = await response.json();
+  let posts = await window.contentPromise;
   if (posts.length > 0) {
     if (category) {
       posts = posts.filter((post) => post.categories.includes(category));
